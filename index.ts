ff9ab8dc87f353b472eb70c92c95c1a20e14c3d0
mtw-programmer <visnia58@gmail.com>
2024-02-05 02:02:33 +0100

    refactor(index): move app setup to services

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
 import './services/mongodb';
+import { setupApp } from './services/app';
 
-import config from './utils/config';
-import log from './utils/log';
-
-import app from './services/express';
-
-const port = config.APP_PORT || 4000;
-export default app.listen(port, () => log.init({ label: 'Index', message: `App listening on port ${port}` }));
+setupApp();
